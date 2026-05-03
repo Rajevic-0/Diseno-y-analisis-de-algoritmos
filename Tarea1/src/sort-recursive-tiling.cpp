e2aa1ac3d44510b7158fc5cfa489e3aaaa2e73eb
Tomas Rajevic <tomaspolp@gmail.com>
2026-05-03 15:02:42 -0400

    feat:Created sort-recursive-tiling.cpp

---
+++ Tarea1/src/sort-recursive-tiling.cpp
@@ -0,0 +1,10 @@
+#include "../include/rtree.hpp"
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+int b = 204;
+std::vector<RTreeNode> nodos(1);
+
+void sort_recursive_tiling() { return; }
