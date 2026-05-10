19a598a6f2d7f513ef41c8440ef9e57c02401051
deragonie <daniela.espinoza.e@ug.uchile.cl>
2026-05-10 19:55:00 -0400

    docs: add documentation to include

--- Tarea1/include/globals.hpp
+++ Tarea1/include/globals.hpp
@@ -3,7 +3,14 @@
 #include "rtree.hpp"
 #include <vector>
 
+/**
+ * @brief Factor determinado por el exacto de cada nodo.
+ */
 extern int b;
+
+/**
+ * @brief Contenedor temporal de nodos durante el bulk-loading
+ */
 extern std::vector<RTreeNode> nodos;
 
 #endif
